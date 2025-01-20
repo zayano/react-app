d5e5a738d6bfa5a2ab7a3f38f7273a295382f68e
Reza Aditya <reza.aditya@pelni.co.id>
2025-01-20 13:31:07 +0700

    showing an alert

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -2,12 +2,19 @@ import { ReactNode } from "react";
 
 interface Props {
   children: ReactNode;
+  onClose: () => void;
 }
 
-const Alert = ({ children }: Props) => {
+const Alert = ({ children, onClose }: Props) => {
   return (
-    <div className="alert alert-primary" role="alert">
+    <div className="alert alert-primary alert-dissmisible">
       {children}
+      <button
+        type="button"
+        className="btn-close"
+        onClick={onClose}
+        data-bs-dismiss="alert"
+      ></button>
     </div>
   );
 };
